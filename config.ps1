22d36294923fd378e22a16517da603db8981ec16
Eric Dombroski <eric@edombroski.com>
2024-04-13 11:10:48 -0400

    Load local config

--- config.ps1
+++ config.ps1
@@ -1,11 +1,30 @@
 # Configurations for folders
-$MonitorConfigFilesLocation     = "$($psscriptroot)\monitor_configs"
-$TestScriptLocation             = "$($psscriptroot)\test_scripts"
-$HelperScriptLocation           = "$($psscriptroot)\helper_scripts"
-$MonitorDataLocation            = "$($psscriptroot)\monitor_data"
+If($env:_MonScriptPath) {
+    $ScriptRoot = $env:_MonScriptPath
+}
+Else {
+    $ScriptRoot = $PSScriptRoot
+}
+
+$MonitorConfigFilesLocation     = "$($ScriptRoot)\monitor_configs"
+$TestScriptLocation             = "$($ScriptRoot)\test_scripts"
+$ActionScriptLocation           = "$($ScriptRoot)\action_scripts"
+$HelperScriptLocation           = "$($ScriptRoot)\helper_scripts"
+$MonitorDataLocation            = "$($ScriptRoot)\monitor_data"
+$LocalConfigFileLocation        = "$($ScriptRoot)\config_local.ps1"
 
 # Configurations for files
-$LastRunFile                    = "$($psscriptroot)\LastRun.txt"
+$LastRunFile                    = "$($ScriptRoot)\LastRun.txt"
 
 # Repeat Action Minutes
 $RepeatActionMinutes            = 12*60
+
+# Default values for Email Action
+$DefaultEmailTo                        = "nobody@localhost"
+$DefaultEmailFrom                      = $env:USERNAME + "@" + $env:COMPUTERNAME
+$DefaultSMTPServer                     = "127.0.0.1"
+
+# Override configurations with local config if present
+If(Test-Path $LocalConfigFileLocation) {
+    . $LocalConfigFileLocation
+}
